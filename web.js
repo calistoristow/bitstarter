199c2003e69c89e02170518e60c7b6fd1312eee8
calistoristow <calisto@me.com>
2013-07-13 21:09:38 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -7,16 +7,11 @@ buf = new Buffer(256);
 
 app.get('/', function(request, response) {
 
+   var buf = new Buffer(fs.readFileSync('index.html','utf8'));
+   response.send(buf.toString());
+
+}); 
  
-  fs.readFile('index.html', function (err, data) {
-    if (err) throw err;
-    response.write(data);   
-  });
-
-  //response.send('Hello World 2!');
-
-});
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
